447e47760af457f8dbb57ffe9e3bb81b31fc1d83
Jekotia <jekotia@jekotia.net>
2012-12-05 18:34:30 -0500

    Database functions are now 100% safe PDO.

--- classes/db.php
+++ classes/db.php
@@ -1,50 +1,50 @@
 <?php
 	class db {
-		public static function connect($auth) {
-			$auth['con'] = 'mysql:host='.$auth['db_host'].';dbname='.$auth['db_name'].';';
+		public static function connect($_) {
+			$_['con'] = 'mysql:host='.$_['db_host'].';dbname='.$_['db_name'].';';
 			try {
-				$con = new PDO($auth['con'],$auth['db_user'],$auth['db_pass']); // mysql
+				$con = new PDO($_['con'],$_['db_user'],$_['db_pass']); // mysql
 			} catch(PDOException $e) {
 				die ('Could not connect to database.'); // Exit, displaying an error message
 			}
 			return $con;
 		}
-		public static function query($auth,$query,$values) {
-			$statement = self::connect($auth)->prepare($query);
-			$statement->execute($values);
-			self::close($statement);
-		}
 		public static function close($statement) {
 			return $statement->closeCursor();
 		}
-		public static function getRow($auth, $table, $column, $value) {
-			$con = self::connect($auth);
-			$query = "SELECT * FROM ".$auth['table_prefix'].$table." WHERE `".$column."` = '".$value."';";
-			//$statement = $con->prepare($query);
-			//$statement->execute();
-			$statement = $con->query($query);
-			$row = $statement->fetch();
-			authdb::close($statement);
-			return $row;
+		public static function query($_, $query, $values) {
+			$statement = self::connect($_)->prepare($query);
+			if(is_array($values)) {
+				$statement->execute($values);
+			} else {
+				$statement->execute(array($values));
+			}
+			self::close($statement);
 		}
-		public static function rowExists($auth, $table, $column, $value) {
-			$con = self::connect($auth);
-			$query = "SELECT COUNT(*) FROM ".$auth['table_prefix'].$table." WHERE `".$column."` = '".$value."';";
-			$statement = $con->prepare($query);
-			$statement->execute();
+		public static function getRow($_, $query, $values) {
+			$statement = self::connect($_)->prepare($query);
+			if(is_array($values)) {
+				$statement->execute($values);
+			} else {
+				$statement->execute(array($values));
+			}
+			$result = $statement->fetch(PDO::FETCH_ASSOC);
+			self::close($statement);
+			return $result;
+		}
+		public static function rowExists($_, $query, $values) {
+			$statement = self::connect($_)->prepare($query);
+			if(is_array($values)) {
+				$statement->execute($values);
+			} else {
+				$statement->execute(array($values));
+			}
 			$count = $statement->fetchColumn(); // investigate switching to rowCount instead of fetchColumn
 			self::close($statement);
-			if ($count !== '1') {
-				return FALSE;
+			if ($count !== '0') {
+				return true;
 			} else {
-				return TRUE;
+				return false;
 			}
-		}
-		public static function updateRow($auth, $table, $column, $value, $findColumn, $findValue) {
-			$query = "UPDATE ".$auth['table_prefix'].$table." SET ".$column."='".$value."' WHERE `".$findColumn."` = '".$findValue."';";
-
-			$statement = self::connect($auth)->prepare($query);
-			$statement->execute();
-			self::close($statement);
 		}
 	}
--- classes/tpl.php
+++ classes/tpl.php
@@ -1,6 +1,7 @@
 <?php
 	class tpl { // functions to ease templating
-		public static function wr($_) { // small foot-print function for returning the web root. For use in echoing it into a HTML document.
+		public static function wr($_) {
+			// small foot-print function for returning the web root. For use in echoing it into a HTML document.
 			echo $_['web_root'];
 		}
 	}
